aaba86d8f158fb3ab24b0c5280822178bcfbfcfa
dami <damikop02@gmail.com>
2023-04-30 01:46:15 +0600

    added login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  // username: string;
+  // password: string;
 
+  constructor(private router: Router) { }
+
+  onSubmit() {
+    // TODO: Implement login logic
+    // For now, just navigate to the home page
+    this.router.navigate(['/home']);
+  }
 }
